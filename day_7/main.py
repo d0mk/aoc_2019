542d8329d8cd1473d8c638f6b62c697add5495dd
d0mk <dominik.kobyra@gmail.com>
2019-12-07 19:36:45 +0100

    part 2 finished

--- day_7/main.py
+++ day_7/main.py
@@ -1,4 +1,4 @@
-from itertools import permutations
+from itertools import permutations, cycle
 import os.path
 import sys
 
@@ -15,33 +15,40 @@ def load_data():
 class IntComputer_v3(IntCodeComputer_v2):
     data = load_data()
 
-    def __init__(self, stdin):
+    def __init__(self, input_data=None):
         super().__init__(IntComputer_v3.data[:])
         self.output = None
-        self.stdin = (n for n in stdin)
+        self.input_data = input_data if type(input_data) == list else None
 
     def input_code(self, i):
-        self.data[i] = next(self.stdin)
+        self.data[i] = self.input_data.pop(0)
         self.ip += 2
 
     def output_code(self, i):
         self.output = self.data[i]
         self.ip += 2
 
+    def add_input_data(self, value):
+        self.input_data.append(value)
+
     def start(self):
-        super().start()
-        return self.output
+        try:
+            super().start()
+        except IndexError:
+            pass
+        finally:
+            return self.output
 
 
-def main():
+def part_1():
     phases = (0, 1, 2, 3, 4)
+
     phase_orders = permutations(phases, len(phases))
     max_thrust = 0
 
     def find_max_thrust(input_signal, phases):
         try:
-            phase = next(phases)
-            amp_out = IntComputer_v3((phase, input_signal)).start()
+            amp_out = IntComputer_v3([next(phases), input_signal]).start()
             return find_max_thrust(amp_out, phases)
         except StopIteration:
             return input_signal
@@ -51,8 +58,38 @@ def main():
         if output > max_thrust:
             max_thrust = output
 
-    print(f'Max thrust: {max_thrust}')
+    print(f'Part 1 max thrust: {max_thrust}')
+
+
+def part_2():
+    phases = (5, 6, 7, 8, 9)
+
+    phase_orders = permutations(phases, len(phases))
+    max_thrust = 0
+
+    for phase_order in phase_orders:
+        # setting phases on every amp
+        amps = [IntComputer_v3([phase]) for phase in phase_order]
+        amps_cycled = cycle(amps)
+
+        amp_input = 0
+
+        # main feedback loop
+        while True:
+            amp = next(amps_cycled)
+            amp.add_input_data(amp_input)
+            amp_input = amp.start()
+
+            # if all of the amps are no longer running, get the
+            # output of the last one (E)
+            if all(a.running == False for a in amps):
+                if (thrust_signal := amps[-1].output) > max_thrust:
+                    max_thrust = thrust_signal
+                break
+
+    print(f'Part 2 max thrust: {max_thrust}')
 
 
 if __name__ == '__main__':
-    main()
+    part_1()
+    part_2()
