f00b777e0c7aee39ccf50a7d92b2d7867109ef81
d0mk <dominik.kobyra@gmail.com>
2019-12-08 15:46:59 +0100

    code reorganisation

--- day_8/main.py
+++ day_8/main.py
@@ -1,14 +1,15 @@
+with open('day_8/input.txt', 'r') as f:
+    data = f.read()
+
+width = 25
+height = 6
+layer_size = width * height
+layers = []
+
+
 def part_1():
-    width = 25
-    height = 6
-    layer_size = width * height
-
-    with open('day_8/input.txt', 'r') as f:
-        data = f.read()
-
     layers = [data[i : i + layer_size] for i in range(0, len(data) - layer_size, layer_size)]
     target_layer = min(layers, key=lambda x : x.count('0'))
-
     print(f'Part 1 result: {target_layer.count("1") * target_layer.count("2")}')
 
 
