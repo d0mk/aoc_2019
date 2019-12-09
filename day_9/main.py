9559851d669609d6e8d75e6f04b75ceaa9781c4f
d0mk <dominik.kobyra@gmail.com>
2019-12-09 12:17:00 +0100

    day 9, initial commit

---
+++ day_9/main.py
@@ -0,0 +1,10 @@
+import os.path
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from day_7.main import IntCodeComputer_v3, load_data
+
+class IntCodeComputer_v4(IntCodeComputer_v3):
+    pass
+
