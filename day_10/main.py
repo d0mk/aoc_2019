057f66c5420ec10fb0a602eff60deb9f0776d6c6
d0mk <dominik.kobyra@gmail.com>
2019-12-10 23:22:32 +0100

    day 10 - part 1 finished

--- day_10/main.py
+++ day_10/main.py
@@ -1,67 +1,30 @@
 import math
 
-
-with open('day_10/input.txt', 'r') as f:
-    data = [line.strip() for line in f.readlines()]
-
-asteroids = [(x, y) for y, _ in enumerate(data) for x, _ in enumerate(data[y]) if data[y][x] == '#']
-
-
-def find_asteroid_axes(origin, candidates):
-    horizontal_axis = [asteroid for asteroid in candidates if asteroid[1] == origin[1]]
-    vertical_axis = [asteroid for asteroid in candidates if asteroid[0] == origin[0]]
-
-    # filter out horizontal and vertical objects
-    for asteroid in horizontal_axis:
-        candidates.remove(asteroid)
-
-    for asteroid in vertical_axis:
-        candidates.remove(asteroid)
-
-    def asteroid_parameters(origin, target):
-        x1, y1 = origin
-        x2, y2 = target
-        a = (y2 - y1) / (x2 - x1)
-        b = (y1 * x2 - y2 * x1) / (x2 - x1)
-        return a, b
-
-    asteroid_parameters = {asteroid : asteroid_parameters(origin, asteroid) for asteroid in candidates}
-    distinct_axes = set(asteroid_parameters.values())
-    grouped_by_axes = {}
-
-    for axis in distinct_axes:
-        grouped_by_axes[axis] = [asteroid for asteroid in asteroid_parameters.keys() if asteroid_parameters[asteroid] == axis]
-
-    if horizontal_axis:
-        grouped_by_axes['horiz'] = horizontal_axis
+def load_data():
+    with open('day_10/input.txt', 'r') as f:
+        data = [line.strip() for line in f.readlines()]
+    return [(x, y) for y, _ in enumerate(data) for x, _ in enumerate(data[y]) if data[y][x] == '#']
     
-    if vertical_axis:
-        grouped_by_axes['vert'] = vertical_axis
 
-    return grouped_by_axes
+def angle(origin, target):
+    return math.atan2(origin[0] - target[0], origin[1] - target[1])
 
 
-asteroid_count = {}
+def part_1():
+    asteroid_count = {}
+    asteroids = load_data()
 
-for asteroid in asteroids:
-    candidates = asteroids[:]
-    candidates.remove(asteroid)
-    grouped_by_axes = find_asteroid_axes(asteroid, candidates)
+    for asteroid in asteroids:
+        candidates = asteroids[:]
+        candidates.remove(asteroid)
 
-    a_x, a_y = asteroid
-    count = len(grouped_by_axes)
+        angles = [angle(asteroid, target) for target in candidates]
 
-    # for k, v in grouped_by_axes.items():
-    #     print(f'{k}: {v}')
+        num_of_asteroids = len(set(angles))
+        asteroid_count[asteroid] = num_of_asteroids
 
-    for key, group in grouped_by_axes.items():
-        if len(group) > 1:
-            t1 = any(a[0] >= a_x and a[1] >= a_y for a in group)
-            t2 = any(a[0] <= a_x and a[1] <= a_y for a in group)
-            if t1 and t2:
-                count += 1
+    print(f'Max asteroids: {max(asteroid_count.values())}')
 
-    asteroid_count[asteroid] = count
 
-best = max(asteroid_count, key=lambda x : asteroid_count[x])
-print(best, asteroid_count[best])
+if __name__ == '__main__':
+    part_1()
